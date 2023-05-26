1242a9258a51430be8e9a978bc83db7de0a63b58
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-26 00:16:44 -0500

    Create AuthenticationController.java

---
+++ prueba/src/main/java/tech/edev404/prueba/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package tech.edev404.prueba.controller;
+
+import org.apache.http.auth.InvalidCredentialsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import tech.edev404.prueba.configuration.security.model.dto.AuthenticationRequest;
+import tech.edev404.prueba.configuration.security.model.dto.AuthenticationResponse;
+import tech.edev404.prueba.configuration.security.service.AuthenticationService;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/auth")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request) throws InvalidCredentialsException {
+            final AuthenticationResponse response = authenticationService.authenticate(request);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+}
